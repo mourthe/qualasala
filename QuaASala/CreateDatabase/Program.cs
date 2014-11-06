7c6fc0f48a72d035cbae7c0bab2448cad4205483
Fabio Mourthé <mourthe@live.com>
2014-11-06 16:46:02 -0200

    Read from the file and put on a list

---
+++ QuaASala/CreateDatabase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create the path and the reader
+            const string path = @"...\..\..\..\docs\Tabela_Salas.csv";
+            var reader = new StreamReader(File.OpenRead(path));
+
+            var rooms = new List<Tuple>();
+            try
+            {
+                // remove the first line
+                reader.ReadLine();
+
+                // begin reading the important part of the file
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    // if the first value is empty continue
+                    if (values[0].Equals(string.Empty)) 
+                        continue;
+
+                    rooms.Add(new Tuple(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]));
+                }
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine("Something went wrong:");
+                Console.WriteLine(exception.Message + "\n" + exception.InnerException);
+            }
+
+            Console.WriteLine("Terminou.");
+            Console.WriteLine("Tamanho: " + rooms.Count);
+            Console.WriteLine("Exemplo: " + rooms.ElementAt(3000).ToString());
+            
+        }
+    }
+}
---
+++ QuaASala/CreateDatabase/Tuple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDatabase
+{
+    public class Tuple
+    {
+        public Tuple(string sala, string horarioIni, string horarioFin, string seg, string ter, string qua, string qui, string sex)
+        {
+            Sex = sex.Contains('1');
+            Qui = qui.Contains('1');
+            Qua = qua.Contains('1');
+            Ter = ter.Contains('1');
+            Seg = seg.Contains('1');
+            HorarioFin = horarioFin;
+            HorarioIni = horarioIni;
+            Sala = sala;
+        }
+
+        public string Sala { get; private set; }
+        public string HorarioIni { get; private set; }
+        public string HorarioFin { get; private set; }
+        public bool Seg { get; private set; }
+        public bool Ter { get; private set; }
+        public bool Qua { get; private set; }
+        public bool Qui { get; private set; }
+        public bool Sex { get; private set; }
+
+        public override string ToString()
+        {
+            return Sala + "," + HorarioIni + "," + HorarioFin + "," + Seg + "," + Ter + "," + Qua + "," + Qui + "," + Sex;
+        }
+    }
+}
