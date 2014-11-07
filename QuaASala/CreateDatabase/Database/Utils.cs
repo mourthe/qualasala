40cd243b77bfab7a78200b2d92f4f4829e231adf
Fabio Mourthé <mourthe@live.com>
2014-11-07 17:00:33 -0200

    Create Database.Utils with ConvertoToDatatable

---
+++ QuaASala/CreateDatabase/Database/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDatabase.Database
+{
+    public class Utils
+    {
+        public static DataTable ConvertToDatatable<T>(IList<T> data)
+        {
+            var props = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+            
+            for (var i = 0; i < props.Count; i++)
+            {
+                var prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            var values = new object[props.Count];
+            foreach (var item in data)
+            {
+                for (var i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+
+            return table;
+        }
+    }
+}
