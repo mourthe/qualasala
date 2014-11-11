e1fa4bae6be89423665772dce5c80371dc2f8651
Fabio Mourthé <mourthe@live.com>
2014-11-11 16:58:11 -0200

    Removes the forbidden rooms

--- QuaASala/CreateDatabase/Database/Utils.cs
+++ QuaASala/CreateDatabase/Database/Utils.cs
@@ -46,5 +46,10 @@ namespace CreateDatabase.Database
 
             return table;
         }
+
+        public static void SaveDatatableOnCSV(DataTable data)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- QuaASala/CreateDatabase/Program.cs
+++ QuaASala/CreateDatabase/Program.cs
@@ -15,9 +15,9 @@ namespace CreateDatabase
             const string path = @"...\..\..\..\docs\Tabela_Salas.csv";
             var reader = new StreamReader(File.OpenRead(path));
 
-            // forbidden room on leme
-            var forbiddenRooms = GetForbiddenRooms("L");
-
+            // forbidden room on PUC
+            var forbiddenRooms = GetForbiddenRooms();
+            
             var rooms = new List<Tuple>();
             try
             {
@@ -30,10 +30,14 @@ namespace CreateDatabase
                     var line = reader.ReadLine();
                     var values = line.Split(',');
 
-                    // if the first value is empty, lab, rdc, dpto or belongs to the forbidden rooms list continue
-                    if (values[0].Equals(string.Empty) || values[0].Equals("RDC") 
-                            || values[0].Equals("LAB") || values[0].Equals("DPTO") 
-                            || forbiddenRooms.Contains(values[0])) 
+                    // if the first value is empty, lab, rdc, dpto or belongs to the forbidden rooms list continue 
+                    if (values[0].Equals(string.Empty) || values[0].Equals("RDC") || values[0].Equals("LAB") 
+                        || values[0].Equals("DPTO") || values[0].ElementAt(0) == 'I' || values[0].ElementAt(0) == 'R' 
+                        || values[0].Equals("LAC") || values[0].ElementAt(0) == 'C' || values[0].ElementAt(0) == 'D'
+                        || forbiddenRooms.Contains(values[0]) || values[0].Equals("LAMAQ") || values[0].Equals("SPA")
+                        || values[0].Equals("FB6") || values[0].Equals("LABHD") || values[0].Equals("LBIAG") 
+                        || values[0].Equals("LIENG") || values[0].Equals("PIUES") || values[0].Contains("ARTE")
+) 
                         continue;
 
                     rooms.Add(new Tuple(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]));
@@ -64,31 +68,41 @@ namespace CreateDatabase
             }
 
             // Create a list from the dictionary
-            var finalData = tuples.Values.ToList();
-            var datatable = Database.Utils.ConvertToDatatable(finalData);
-
-            Console.WriteLine("Terminou.");
+            var finalData = tuples.Values.ToList().OrderBy(x=>x.Sala).ToList();
+            //var datatable = Database.Utils.ConvertToDatatable(finalData);
+            
             Console.WriteLine("Tamanho lista: " + rooms.Count);
             Console.WriteLine("Tamanho dicionario: " + tuples.Keys.Count);
             Console.WriteLine("Exemplo lista: " + rooms.ElementAt(3000));
             Console.WriteLine("Exemplo dicionario: " + tuples.ElementAt(501));
+
+            // save the datatable on a csv file
+            // Database.Utils.SaveDatatableOnCSV(datatable);
+
+            Console.WriteLine("Terminou.");
         }
 
 
-        private static List<string> GetForbiddenRooms(string prefix)
+        private static List<string> GetForbiddenRooms()
         {
             // create the path and the reader for the forbidden rooms
-            const string path = @"...\..\..\..\docs\Sala_Proibidas_Leme.txt";
+            var result = new List<string>();
+            GetForbiddenRoomsForBuilding(result, @"...\..\..\..\docs\Sala_Proibidas_Leme.txt", "L");
+            GetForbiddenRoomsForBuilding(result, @"...\..\..\..\docs\Sala_Proibidas_Kennedy.txt", "K");
+            //GetForbiddenRoomsForBuilding(out result, @"...\..\..\..\docs\Sala_Proibidas_Leme.txt", "L");
+
+            return result;
+        }
+
+        private static void GetForbiddenRoomsForBuilding(List<string> rooms, string path, string prefix)
+        {
             var reader = new StreamReader(File.OpenRead(path));
 
             // put the content of the file into a list
-            var rooms = new List<string>();
             while (!reader.EndOfStream)
             {
                 rooms.Add(prefix + reader.ReadLine());
             }
-            
-            return rooms;
         }
     }
 }
